fc2bb06e255f4fe9bb3dc03aecad84f54d72f429
katrinawightman <katrina.wightman@outlook.com>
2023-10-24 14:45:17 -0400

    Update to handsOnExercise1

--- KWightman/handsOnExercise1.py
+++ KWightman/handsOnExercise1.py
@@ -1,7 +1,8 @@
 #Print shopping list
 shoplist = ['apples','oranges','bananas']
-print("Shopping List:\n")
+print("\nShopping List:\n")
 print(','.join(map(str, shoplist)))
 #Add to shopping list and print
 shoplist.append("the function added this")
+print("\nShopping List:\n")
 print(','.join(map(str, shoplist)))
