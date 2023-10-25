cb05314f24d9e56df28c168401417afeb5cfd5fc
Artem Cherednichenko <51989411+BC-ACherednichenko@users.noreply.github.com>
2023-10-24 20:23:00 -0400

    add pseudocode for rolldice game (#16)

---
+++ acherednichenko/rollTheDicePseudocode.py
@@ -0,0 +1,23 @@
+# the app starts
+
+# entities or classes of two players are created and the total points of 100 are assigned for each player
+
+# entity or classes of a round (or a roll) where wagers of players are added is initialised, rounds will have
+# attributes of the winner and wager and dice value
+
+# create a gameplay class that is responsible for generating rand number as the dice value
+# when classes are created we start the initialisation of the cli part of the app
+
+# initialize the classes
+# start the gameplay loop
+    # collect config from gameplay (amount of rounds), and setup it as loop params (how many iterations we are going to have)
+    # start gaming loop cycles
+        # show the points balance for every player
+        #  collect the wagers from players via prompt cli params for round N
+        # trigger the gameplay roll dice method
+        # define the winner
+        # subtract the amt from loser and assign to the winner
+        # assign to wager to a winner
+        # go to the step 1 until number of rounds is over
+    # end of the loop
+# end of the loop if exit is cliecked
