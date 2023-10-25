ce19c81ef0d92c149f4eb8b20075c11bca9bd634
katrinawightman <148288628+katrinawightman@users.noreply.github.com>
2023-10-24 20:24:56 -0400

    Add files via upload (#19)

---
+++ DiceGame.py
@@ -0,0 +1,10 @@
+#Dice Game intro
+
+#Start with 100 pts
+#If 7 or 11 is rolled, player wins wager and points are added to total
+#If 2,3, 0r 12 is rolled, player loses wager and points are subtracted from total
+#Player wins when points reach 1000, or loses when points reach 0
+
+#Display points balance prior to each turn
+#Prompt player to enter a wager
+#Simulate rolling 2 6-sided dice using random.randint(1, 6)
