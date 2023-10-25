d4cc1b0e6120f393b0e07f8ad7f3550d3fa0bbb8
erolyigitalp <eyigitalp@mail.endicott.edu>
2023-10-24 20:26:33 -0400

    Dice Game Pseudocode

---
+++ erolyigitalp/myDiceGame.py
@@ -0,0 +1,22 @@
+# Print user balance
+# Ask user to start game
+
+    # Start game
+
+        # Ask user how much they want to bet
+        # Roll first dice - create random number between 1-6
+        # Roll second dice - create random number between 1-6
+        # Sum 2 created random number and print screen
+
+            # If sum of 2 dice 7 or 11
+                # Print player win
+                    # Add 2x balance of played balance to user total balance
+                    # If user balance >= 100
+                        # Close game
+                    # Else ask user how much they want to bet
+
+            # If sum of 2 dice 2 or 3 or 12
+                # Print player lost
+                    # If user balance <= 0
+                        # Close game
+                    # Else ask user how much they want to bet
