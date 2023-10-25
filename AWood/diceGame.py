2b87b5997adee33e4f6948a1edd62fafd003c03e
NiteLite2 <127334034+NiteLite2@users.noreply.github.com>
2023-10-24 20:27:43 -0400

    Pseudo Code (Template/Dice Game) (#20)

---
+++ AWood/diceGame.py
@@ -0,0 +1,28 @@
+# Determine player wages between 1 and total 
+
+# Players roll 2 dice until (total)
+    # 3
+    # 7
+    # 11
+    # or 12
+
+# Player wins if 
+    # 7 rolled
+    # 11 rolled
+
+# Player loses if
+    # 2 rolled
+    # 3 rolled
+    # 12 rolled
+
+# Game ends when
+    # 1000 points (total)
+
+# Template for Player Score
+    # 0-1000
+
+# Print results for following
+    # You Win! Huzzah!
+    # Da Feeted! Buy some Jordans and try again :) 
+
+# Define a function (roll dice)
