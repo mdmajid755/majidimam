59f7720d2123a1a425ee4681b30df9710aac6e8e
atothEndicott <127053188+atothEndicott@users.noreply.github.com>
2023-10-24 20:10:31 -0400

    Dice game pseudocode (#17)

---
+++ Prof/diceGame.py
@@ -0,0 +1,30 @@
+# Print introduction for player
+
+# Start Game loop
+
+    # prompt player with player point total
+    # Prompt player for wager
+
+    # Trigger dice roll
+
+    # calculate dice total
+
+    #prompt the player with dice total
+
+    # if dice in winning values
+        # add wager to player total
+        # if player has won game
+            # print player wins message
+
+            # break out of game loop
+    # elif dice in losing values
+        #subtract from player total
+        #if player has lost game
+            # print player loses message
+            # break out of game loop
+
+    
+
+
+
+ 
