bc9fcc896185e01672b90c9ac6d8f7b5fb5866a3
erolyigitalp <eyigitalp@mail.endicott.edu>
2023-10-31 14:21:22 -0400

    Edited one of Dice file

--- erolyigitalp/mainDice.py
+++ erolyigitalp/mainDice.py
@@ -6,9 +6,23 @@ class Dice:
 
     def roll(self):
         return random.randint(1, self.sides)
-    
-if __name__ == "__main__":
-    gameStart()
 
 def gameStart():
     playerBalance = 100
+    while playerBalance > 0 and playerBalance < 1000:
+        status = oneRound(playerBalance)
+        if status == "exited":
+            break
+        playerBalance += status
+    if status == "exited":
+        print("Exiting...")
+    elif playerBalance >= 1000:
+        print("Congratulation! You won max win!")
+    elif playerBalance <= 0:
+        print("You lost your all money. Better luck next time.")
+
+def oneRound(playerBalance):
+    print("You have $" + playerBalance + ".")
+
+if __name__ == "__main__":
+    gameStart()
